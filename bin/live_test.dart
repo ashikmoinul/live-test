be53153083cb743bbc48fdb86c36f3bf169c2b5e
A S H I K <ashik.moinul150@gmail.com>
2024-03-15 23:58:13 +0600

    Code Added

--- bin/live_test.dart
+++ bin/live_test.dart
@@ -1,4 +1,27 @@
-void main(){
-  print('Ashik');
-  print('Tuhin');
+void main() {
+  List<Map<String, dynamic>> fruits = [
+    {'name': 'Apple', 'color': 'Red', 'price': 2.5},
+    {'name': 'Banana', 'color': 'Yellow', 'price': 1.0},
+    {'name': 'Grapes', 'color': 'Purple', 'price': 3.0},
+  ];
+
+  print('Original Fruit Details before Discount');
+  displayFruitDetails(fruits);
+
+  applyPriceDiscount(fruits, 10);
+
+  print('Fruit Details After Applying 10% Discount');
+  displayFruitDetails(fruits);
 }
+
+void applyPriceDiscount(List<Map<String, dynamic>> fruits, int i) {
+}
+
+void displayFruitDetails(List<Map<String, dynamic>> fruits) {
+  for (var fruit in fruits) {
+    print('Name: ${fruit['name']}, Color: ${fruit['color']}, Price: ${fruit['price']}');
+  }
+}
+
+
+
